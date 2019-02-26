035846beb0aa3452c61e764abc2706519eb29f3f
Kyler <Krenneker16@gmail.com>
2019-02-26 17:31:52 -0500

    added facebook sdk JS

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log('Our JS file is connected!');
+
+
+
+window.fbAsyncInit = function() {
+FB.init({
+    appId      : '2079901702307820',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v3.2'
+});
+    
+FB.AppEvents.logPageView();   
+    
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+FB.getLoginStatus(function(response) {
+statusChangeCallback(response);
+console.log(response);
+});
