c47c60d84370ba99d7b6f140e14cae38405195e0
Kyler <Krenneker16@gmail.com>
2019-02-26 17:35:15 -0500

    added facebook sdk JS v3

--- main.js
+++ main.js
@@ -11,11 +11,7 @@ FB.init({
 });
     
 FB.AppEvents.logPageView();   
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-    console.log(response);
-    });
+    
 };
 
 (function(d, s, id){
