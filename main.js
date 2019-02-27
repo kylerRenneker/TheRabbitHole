a059d5f026b67a1308ae334f84a5bb89e0a0edbb
Kyler <Krenneker16@gmail.com>
2019-02-26 19:58:49 -0500

    removed previous work to start two new branches

--- main.js
+++ main.js
@@ -1,27 +1,2 @@
-console.log('Our JS file is connected!');
 
 
-
-window.fbAsyncInit = function() {
-FB.init({
-    appId      : '2079901702307820',
-    cookie     : true,
-    xfbml      : true,
-    version    : 'v3.2'
-});
-    
-FB.AppEvents.logPageView();   
-    
-};
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-FB.login(function(response) {
-    console.log(response);
-  }, {scope: 'publish_video'});
