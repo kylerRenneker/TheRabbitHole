898f41fc0913a026fb92b186e9e3926d59c9eeef
Kyler <Krenneker16@gmail.com>
2019-02-27 18:13:45 -0500

    no change

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+$('.js-randomize').on('click', function(){
+    player.loadVideoById('RCXGpEmFbOw')
+    // player.videoId = 'RCXGpEmFbOw';
+    console.log(player.videoId);
+  })
 
+  console.log(player.videoId);
 
