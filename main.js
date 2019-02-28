80015c04cd7dfc8f7ba60160a64dca860842373f
Kyler <Krenneker16@gmail.com>
2019-02-27 22:08:32 -0500

    commented out randomId function

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
+console.log(firstScriptTag);
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
@@ -38,11 +38,27 @@ function onPlayerReady(event) {
 //   player.stopVideo();
 // }
 
+let randomVid = 'oOPVBm0sA7Q';
+
 $('.js-randomize').on('click', function(){
-    player.loadVideoById('RCXGpEmFbOw')
+    
+  
+  player.loadVideoById(randomVid);
     // player.videoId = 'RCXGpEmFbOw';
     console.log(player.videoId);
   })
 
-  console.log(player.videoId);
 
+
+// function randomId() {
+//   const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_';
+//   const strLength = 11;
+//   let randomStr = '';
+//   for (let i = 0; i<strLength; i++){
+//     let rId = Math.floor(Math.random() * chars.length);
+//     randomStr += chars.substring(rId, rId+1);
+//   }
+//   return randomStr;
+// }
+
+// console.log(randomId());
