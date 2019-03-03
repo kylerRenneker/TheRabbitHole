0f4bd3823004ebcde5ac52ed50bbe32da59dcb17
Kyler <Krenneker16@gmail.com>
2019-03-03 15:02:34 -0500

    adding pagination

--- main.js
+++ main.js
@@ -1,86 +1,130 @@
+'use strict'
 
-const apiKey = 'AIzaSyCtiVFVUXm6cOL5PjFl64liwXOOkTYG9FA';
-const rootURL = 'https://www.googleapis.com/youtube/v3/search';
+const baseURL = 'https://www.googleapis.com/youtube/v3/videos'
+const apiKey = 'AIzaSyAhui6AUkhaT17er7V3Q1kwvmHV_kSdumM'
 
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
 
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-console.log('created tag: ', tag);
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-console.log('first script tag: ', firstScriptTag);
-console.log('tag inserted above first script tag: ',tag);
+function getNextVideoInfo(category) {
+  const params = {
+    part: 'snippet',
+    videoCategoryId: category,
+    chart: 'mostPopular',
+    regionCode: 'US',
+    key: apiKey,
+    maxResults: '50',
+    pageToken: 'CDIGAA'
+  }
 
-// This function creates an <iframe> (and YouTube player)
-// after the API code downloads.
-var player;
+  console.log(formatQueryParams(params));
+  const queryString = formatQueryParams(params)
+  const url = baseURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+  .then (response => {response.ok
+    if (response.ok){
+      return response.json();
+    }
+    throw new Error (response.statusText);
+})
+  // .then(responseJson => getNextPage(responseJson))
+  .then(responseJson => {
+      params.pageToken = responseJson.nextPageToken;
+      console.log(params.pageToken);
+      getRandomId(responseJson)
+    })
+  .catch(err => {
+    alert(`Something went wrong: ${err.message}`);
+  })
+}
+
+function getRandomId(responseJson) {
+  console.log(responseJson);
+  let randomItem = responseJson.items[Math.floor(Math.random() * responseJson.items.length)];
+  console.log(randomItem);
+  playNext(randomItem);
+  console.log(responseJson.nextPageToken)
+}
+
+function playNext(randomVideo) {
+  player.loadVideoById(randomVideo.id);
+}
+
+function loadIframe() {
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
     videoId: 'M7lc1UVf-VE',
     events: {
-      'onReady': onPlayerReady
-      //'onStateChange': onPlayerStateChange
+      'onReady': onPlayerReady,
+      // 'onStateChange': onPlayerStateChange
     }
   });
 }
 
-// The API will call this function when the video player is ready.
+// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.stopVideo();
+  console.log(player.getPlayerState());
 }
-  
-// Populate dropdown with list of categories
-const dropdown = document.getElementById('category-list');
-dropdown.length = 0;
 
-const defaultOption = document.createElement('option');
-defaultOption.text = 'All videos';
+function loadDropdown() {
+$('#category-list').append($(`<option value="0">All videos</option>`));
+}
 
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
-
-function getCategories(){
-  fetch('https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=AIzaSyCtiVFVUXm6cOL5PjFl64liwXOOkTYG9FA')  
-    .then(  
-      function(response) {  
-        if (response.status !== 200) {  
-          console.warn('Looks like there was a problem. Status Code: ' + 
-            response.status);  
-          return;  
-        }
-
-        // Examine the text in the response  
-        response.json().then(data => {  
-          let option;
-          for (let i = 0; i < data.items.length; i++) {
-            option = document.createElement('option');
-            option.text = data.items[i].snippet.title;
-            option.value = data.items[i].id;
-            dropdown.add(option);
-        }    
-        });  
+function getCategories(){ //remove or hide 30-44
+  fetch('https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=AIzaSyAhui6AUkhaT17er7V3Q1kwvmHV_kSdumM')  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + response.status);  
+        return;  
       }
-    )
-    
-    .catch(err => {  
+// Examine the text in the response  
+      response.json()
+      .then(data => { 
+      	for (let i = 1; i < data.items.length; i++) {
+          let text = data.items[i].snippet.title;
+          let value = data.items[i].id;
+          $('#category-list').append(`<option value=${value}>${text}</option>`)   
+    	  }    
+      });  
+    }  
+  )  
+  .catch(err => {  
       console.error('Fetch Error -', err);  
-    });
+  });
 }
 
 function formSubmit(){
-  $('#btn').on('click', function(){
+  $('form').on('submit', function(event){
     event.preventDefault();
-    player.loadVideoById(randomVid);
-      console.log(player.videoId);
-    })
+    let category = $('#category-list').val();
+    getNextVideoInfo(category);
+  })
 }
 
 function loadPage() {
   getCategories();
   formSubmit();
+  loadIframe();
+  loadDropdown();
 }
 
 $(loadPage);
