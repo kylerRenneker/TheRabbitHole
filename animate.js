426a38887ab5cb08d8d698200f74030ac8d770ec
Kyler <Krenneker16@gmail.com>
2019-03-10 00:36:41 -0500

    added drop down for mobile and nav links for desktop

--- animate.js
+++ animate.js
@@ -2,7 +2,24 @@ $(window).scroll(function(){
     if($(window).scrollTop() > 100) {
         $('.topHeader').addClass('sticky');
     }
+    else if($('.navLinks').hasClass('open-nav')){
+        $('.navLinks').removeClass('open-nav');
+        $('.topHeader').removeClass('nav-on-click');
+    }
     else {
         $('.topHeader').removeClass('sticky');
+    }
+})
+
+$('.mobile-nav').on('click', function(){
+    if($('.navLinks').hasClass('open-nav')){
+        $('.navLinks').removeClass('open-nav')
+        $('.topHeader').removeClass('nav-on-click');
+    }
+    else {
+        $('.navLinks').addClass('open-nav');
+        $('.topHeader').addClass('nav-on-click');
+        $('.navLinks').addClass('mobile-nav-links');
+        $('.navLinks li').addClass('mobile-li');
     }
 })
